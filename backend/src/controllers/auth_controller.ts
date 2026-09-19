63bd7bb94ae3f0a265e23bf57c3f44596f31c0e8
Muhammad-Ahmad618 <muhammad.ahmad.86.18@gmail.com>
2026-09-19 14:30:24 +0500

    changed login controller configurations

--- backend/src/controllers/auth_controller.ts
+++ backend/src/controllers/auth_controller.ts
@@ -69,14 +69,14 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("accessToken", accesstoken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshtoken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
