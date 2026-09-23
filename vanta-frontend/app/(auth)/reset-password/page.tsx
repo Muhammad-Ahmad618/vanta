826c956d711ac8193fc6f85dcd5b92f6de768c01
Muhammad-Ahmad618 <muhammad.ahmad.86.18@gmail.com>
2026-09-23 21:56:55 +0500

    added suspense component around reset form

--- vanta-frontend/app/(auth)/reset-password/page.tsx
+++ vanta-frontend/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,13 @@
 import ResetPasswordFrom from "@/components/features/auth/resetPasswordForm";
+import { Suspense } from "react";
 
 function ResetPasswordpage() {
   return (
     <div className="flex h-screen justify-center items-center p-4">
       <div className="max-w-[500px] w-full">
-        <ResetPasswordFrom />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ResetPasswordFrom />
+        </Suspense>
       </div>
     </div>
   );
